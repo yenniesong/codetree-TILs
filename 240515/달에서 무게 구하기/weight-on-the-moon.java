cbf051eca25921e5e6a6c4e1332f186aa956a5b0
yenniesong <102870946+yenniesong@users.noreply.github.com>
2024-05-15 02:46:49 +0900

    Title: 달에서 무게 구하기, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240515/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 13;
+        double m = 0.165;
+        System.out.printf(a + " * " + "%.6f" + " = " + "%.6f", m, (a*m));
+    }
+}
