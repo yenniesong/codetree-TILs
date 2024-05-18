ade9883e0133f3b92086a00eadafa231044d3824
yenniesong <102870946+yenniesong@users.noreply.github.com>
2024-05-19 00:43:27 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240519/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
+++ 240519/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -1,27 +1,30 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int arr[] = new int[11];
+        // 배열 선언
+        int[] arr = new int[100];
+        int[] newArr = new int[100];
+        int zeroPoint = 0;
 
-        for(int i = 0; i < 11; i++) {
+        // 입력받은 정수로부터 출력할 값을 새로운 배열에 저장하고, 0이 나오면 for문을 탈출
+        for(int i = 0; i < 100; i++) {
             arr[i] = sc.nextInt();
-        }
-
-        for(int i = 0; i < arr.length; i++) {
-
-            if(arr[i] % 2 == 0){
-                arr[i] = arr[i] / 2;
-            } else {
-                arr[i] = arr[i] + 3;
-            }
-
             if(arr[i] == 0) {
+                zeroPoint = i;
                 break;
             }
-            
-            System.out.print(arr[i] + " ");
+            if(arr[i] % 2 == 0)
+                newArr[i] = arr[i] / 2;
+            else
+                newArr[i] = arr[i] + 3;
+        }
+
+        // 0이 나오기 전까지의 배열의 인덱스에 저장된 값을 출력
+        for(int i = 0; i < zeroPoint; i++) {
+            System.out.print(newArr[i] + " ");
         }
     }
 }
