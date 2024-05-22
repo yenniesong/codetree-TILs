e8c4c0bc491855d3125ef8f05ccd13a9b84e9314
yenniesong <102870946+yenniesong@users.noreply.github.com>
2024-05-22 20:38:33 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 187ms, Memory: 13MB, Status: Passed - CodeTree

--- 240522/중복되지 않는 숫자 중 최대/max-of-unique-number.java
+++ 240522/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -1,35 +1,37 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int arr[] = new int[n];
+        // 첫 번째 줄: 원소의 수 N
+        int N = sc.nextInt();
 
-        int max = Integer.MIN_VALUE;
-        int cnt = 0;
-        int idx = -1;
-        for(int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        // 두 번째 줄: N개의 숫자
+        int[] numbers = new int[N];
+        for (int i = 0; i < N; i++) {
+            numbers[i] = sc.nextInt();
+        }
 
-            if(arr[i] > max) {
-                // System.out.println("arr[i] > max : " + max);
-                max = arr[i];
-            } else if(arr[i] == max) {
-                cnt++;
-                // System.out.println("arr[i] == max : " + max);
-                // System.out.println("cnt : " + cnt);
-                // idx = -1
+        // 등장 횟수를 저장할 배열 (숫자의 범위는 1~1000)
+        int[] count = new int[1001];
+
+        // 각 숫자의 등장 횟수를 계산
+        for (int i = 0; i < N; i++) {
+            count[numbers[i]]++;
+        }
+
+        // 중복하지 않는 숫자 중 최댓값을 찾기
+        int maxUnique = -1;
+        for (int i = 1; i <= 1000; i++) {
+            if (count[i] == 1) {
+                if (i > maxUnique) {
+                    maxUnique = i;
+                }
             }
-            
-            
+        }
 
-        }
-        if(cnt > 0) {
-            idx = -1;
-            System.out.print(idx);
-        } else {
-            System.out.print(max);
-        }
+        // 결과 출력
+        System.out.println(maxUnique);
     }
 }
