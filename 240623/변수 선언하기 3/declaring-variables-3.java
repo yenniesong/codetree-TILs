4084f664212c685ca0d6d495f587090be1b61afe
yenniesong <102870946+yenniesong@users.noreply.github.com>
2024-06-23 19:14:47 +0900

    Title: 변수 선언하기 3, Time: 79ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240623/변수 선언하기 3/declaring-variables-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 6;
+        int c = 9;
+
+        System.out.print(a + " + " + b + " = " + c);
+    }
+}
