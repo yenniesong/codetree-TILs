50e5722caf5b30d34cc84d0c5af4344f50816713
yenniesong <102870946+yenniesong@users.noreply.github.com>
2024-06-23 19:13:54 +0900

    Title: 틀린 덧셈식, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240623/틀린 덧셈식/incorrect-addition-expression.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 123;
+        int b = 43;
+        int c = 80;
+
+        System.out.print(a + " + " + b + " = " + c);
+    }
+}
